376a97aca7bddb95b5edd30cb7874922dc645d3b
fajarikas <fajarika.2021@student.uny.ac.id>
2024-11-06 19:19:17 +0700

    nf all function done

---
+++ app/Filament/Resources/AnnouncementResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AnnouncementResource\Pages;
+use App\Filament\Resources\AnnouncementResource\RelationManagers;
+use App\Models\Announcement;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AnnouncementResource extends Resource
+{
+    protected static ?string $model = Announcement::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->label('Judul')
+                    ->required()
+                    ->columnSpan('full'),
+                FileUpload::make('detail')
+                    ->label('Pengumuman')
+                    ->disk('public')
+                    ->preserveFilenames()
+                    ->directory('uploads/announcement')
+                    ->required()
+                    ->columnSpan('full')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->label('Judul'),
+                TextColumn::make('detail')
+                    ->label('File Pengumuman')
+                    ->url(fn($record) => asset('storage/' . $record->detail)) // Mengambil URL dengan benar
+                    ->formatStateUsing(fn($state) => basename($state)) // Hanya ambil nama file
+                    ->openUrlInNewTab()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAnnouncements::route('/'),
+            'create' => Pages\CreateAnnouncement::route('/create'),
+            'edit' => Pages\EditAnnouncement::route('/{record}/edit'),
+        ];
+    }
+}
